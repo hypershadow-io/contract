bc2b254f36ccb798133f9579b6e2d5c6f5c99bdb
rathil <shadow_exe@ukr.net>
2025-07-15 18:45:27 +0200

    Init apitoken

---
+++ apitoken/api.go
@@ -0,0 +1,8 @@
+package apitoken
+
+import (
+	"github.com/hypershadow-io/contract/entity"
+)
+
+// EntityType is the global identifier for the API token entity type.
+const EntityType entity.Type = "api-token"
