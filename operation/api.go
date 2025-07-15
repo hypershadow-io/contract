3a7b24d0c222763530928a21c7bac8fc0abe8eff
rathil <shadow_exe@ukr.net>
2025-07-15 15:00:45 +0200

    Init operation

---
+++ operation/api.go
@@ -0,0 +1,8 @@
+package operation
+
+import (
+	"github.com/hypershadow-io/contract/entity"
+)
+
+// EntityType is the global identifier for the operation entity type.
+const EntityType entity.Type = "operation"
