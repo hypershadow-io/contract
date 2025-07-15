3ccb7e75da53dfdd7b7ca6c7a92fe1c18cde1fb2
rathil <shadow_exe@ukr.net>
2025-07-15 14:10:44 +0200

    Init integration

---
+++ integration/api.go
@@ -0,0 +1,8 @@
+package integration
+
+import (
+	"github.com/hypershadow-io/contract/entity"
+)
+
+// EntityType is the global identifier for the integration entity type.
+const EntityType entity.Type = "integration"
---
+++ integration/go.mod
@@ -0,0 +1,5 @@
+module github.com/hypershadow-io/contract/integration
+
+go 1.24.0
+
+require github.com/hypershadow-io/contract/entity v1.0.0
