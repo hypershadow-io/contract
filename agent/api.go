7d7846ced1181c0c39d6c0281bc0fd2535a44889
rathil <shadow_exe@ukr.net>
2025-07-15 10:43:50 +0200

    Update agent contracts

--- agent/api.go
+++ agent/api.go
@@ -3,32 +3,11 @@ package agent
 import (
 	"context"
 
-	"github.com/hypershadow-io/contract/agent/model"
 	"github.com/hypershadow-io/contract/entity"
-	"github.com/hypershadow-io/contract/hook"
-	"github.com/hypershadow-io/contract/qb"
 )
 
 // Client defines the base interface for working with Agent.
 type Client interface {
-	// ModelHook returns a mutator registry for agent models associated with the given plugin ID.
-	ModelHook(pluginID string) hook.Mutator[model.Model]
-
-	// ModelEvent returns an event registry for agent models associated with the given plugin ID.
-	ModelEvent(pluginID string) hook.Event[model.Model]
-
-	// SQLSelectHook returns a mutator registry for SELECT SQL queries related to agents.
-	SQLSelectHook(pluginID string) hook.Mutator[qb.SelectQuery]
-
-	// SQLInsertHook returns a mutator registry for INSERT SQL queries related to agents.
-	SQLInsertHook(pluginID string) hook.Mutator[qb.InsertQuery]
-
-	// SQLUpdateHook returns a mutator registry for UPDATE SQL queries related to agents.
-	SQLUpdateHook(pluginID string) hook.Mutator[qb.UpdateQuery]
-
-	// SQLDeleteHook returns a mutator registry for DELETE SQL queries related to agents.
-	SQLDeleteHook(pluginID string) hook.Mutator[qb.DeleteQuery]
-
 	// IDFromContext extracts the agent ID from the given context.
 	IDFromContext(c context.Context) int64
 
--- agent/go.mod
+++ agent/go.mod
@@ -2,16 +2,4 @@ module github.com/hypershadow-io/contract/agent
 
 go 1.24.0
 
-require (
-	github.com/hypershadow-io/contract/agent/model v1.0.0
-	github.com/hypershadow-io/contract/entity v1.0.0
-	github.com/hypershadow-io/contract/hook v1.0.0
-	github.com/hypershadow-io/contract/qb v1.0.0
-)
-
-require (
-	github.com/hypershadow-io/contract/db v1.0.0 // indirect
-	github.com/hypershadow-io/contract/eb v1.0.0 // indirect
-	github.com/hypershadow-io/contract/meta v1.0.0 // indirect
-	github.com/hypershadow-io/contract/utiliter v1.0.0 // indirect
-)
+require github.com/hypershadow-io/contract/entity v1.0.0
