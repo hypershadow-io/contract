a641bd8448f8e04b7523168cd2d0e9310bd00b6d
rathil <shadow_exe@ukr.net>
2025-07-30 12:13:08 +0200

    Update meta/json

--- meta/json/api.go
+++ meta/json/api.go
@@ -1,14 +1,14 @@
 package json
 
 import (
-	"github.com/hypershadow-io/contract/json"
+	"github.com/hypershadow-io/contract/json/raw"
 	"github.com/hypershadow-io/contract/meta"
 )
 
 // Meta is a network-safe representation of meta.Meta,
 // designed for receiving structured metadata over the network (e.g., via HTTP)
 // before transforming it into the internal Meta type.
-type Meta map[string]json.RawMessage
+type Meta map[string]raw.Message
 
 // Meta converts json.Meta into the internal meta.Meta type.
 func (a Meta) Meta() meta.Meta {
--- meta/json/go.mod
+++ meta/json/go.mod
@@ -3,8 +3,6 @@ module github.com/hypershadow-io/contract/meta/json
 go 1.24.0
 
 require (
-	github.com/hypershadow-io/contract/json v1.0.0
+	github.com/hypershadow-io/contract/json/raw v1.0.0
 	github.com/hypershadow-io/contract/meta v1.0.0
 )
-
-require github.com/hypershadow-io/contract/codec v1.0.0 // indirect
