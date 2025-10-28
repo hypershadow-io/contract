96860aab78f88739f415b4cccf6b9fdf3ad4d024
rathil <shadow_exe@ukr.net>
2025-10-28 12:24:58 +0100

    Change types in setauth.SetAuth

--- operation/setauth/api.go
+++ operation/setauth/api.go
@@ -5,9 +5,9 @@ import (
 )
 
 // Client defines an interface for injecting authorization values into the parameter set
-// according to the schema definition.
+// according to the property definition.
 type Client interface {
 	// SetAuth populates the provided parameters with authorization values
-	// based on the rules defined in the schema.
-	SetAuth(sch schema.Schema, params any, auth map[string]string) any
+	// based on the rules defined in the property.
+	SetAuth(prop schema.Property, params any, auth map[string]string) any
 }
